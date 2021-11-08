f22d414ed1fcdb55942578eabd48be5717b06a32
KingaB <tcx7@o2.pl>
2021-11-08 17:24:24 +0100

    Add periodic boarder conditions

--- src/main/java/simulation/Vector2D.java
+++ src/main/java/simulation/Vector2D.java
@@ -9,6 +9,15 @@ public class Vector2D {
         this.x = x;
         this.y=y;
     }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     @Override
     public String toString() {
         return String.format("(%d, %d)", x, y);
